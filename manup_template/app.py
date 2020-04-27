75dbe5e2941413e719807619904eda15244fa262
SimonSkade <simon.skade@gmx.net>
2020-04-27 21:19:27 +0200

    created forms for login and register, but there is no database yet. There are also some little style issues that will be fixed at the end.

--- manup_template/app.py
+++ manup_template/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "bb31594820cb0ee8d7e0dc07bd156619"
+
+
 @app.route("/")
-def index():
+@app.route("/home")
+def home():
 	return render_template("index.html")
  
 
@@ -11,6 +16,21 @@ def index():
 def about():
 	return render_template("about.html")
 
+@app.route("/register", methods=["GET","POST"])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f"Account created for {form.username.data}!", "success")
+		return redirect(url_for("home"))
+	return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		#here is an placeholder
+		redirect("home")
+	return render_template("login.html", form=form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
