a478a4a533a5205f676ddaf239fcb26882cd9851
SimonSkade <simon.skade@gmx.net>
2020-06-07 10:06:05 +0200

    fixed little mistake

--- physicscontests/__init__.py
+++ physicscontests/__init__.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 
-if os.environ.get("ENV") == production:
+if os.environ.get("ENV") == "production":
 	app.config["DEBUG"] = False
 	app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
